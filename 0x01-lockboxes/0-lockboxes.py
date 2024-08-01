f9c26aab6775902e73cb414a3a07e21056af00a2
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-08-02 02:59:56 +0300

    add: lockboxes task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lockboxes"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Parameters:
+    boxes (list of list of int): A list where each index represents a box and the values at each index represent the keys contained in that box.
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+    
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+    
+    return all(visited)
