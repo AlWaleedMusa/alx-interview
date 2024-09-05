3f4189b063ce7c727d524c778f45f336daed74d5
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-05 20:37:28 +0300

    fix: task 0 code refctored and fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,15 +4,15 @@ const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
   if (error) throw error;
-  const movie_actors = JSON.parse(body).characters;
-  exactOrder(movie_actors, 0);
+  const movieActors = JSON.parse(body).characters;
+  exactOrder(movieActors, 0);
 });
 
-const exactOrder = (movie_actors, x) => {
-  if (x === movie_actors.length) return;
-  request(movie_actors[x], function (error, response, body) {
+const exactOrder = (movieActors, x) => {
+  if (x === movieActors.length) return;
+  request(movieActors[x], function (error, response, body) {
     if (error) throw error;
     console.log(JSON.parse(body).name);
-    exactOrder(movie_actors, x + 1);
+    exactOrder(movieActors, x + 1);
   });
 };
